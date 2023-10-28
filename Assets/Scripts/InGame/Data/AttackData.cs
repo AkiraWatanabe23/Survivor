83a43729f04323924e84c4bc972b80c6f83407c8
AkiraWatanabe23 <a0323w@gmail.com>
2023-10-28 16:45:42 +0900

    [add] 攻撃系システム基盤

---
+++ Assets/Scripts/InGame/Data/AttackData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AttackData : MonoBehaviour
+{
+    [SerializeField]
+    private int _attackValue = 1;
+    [SerializeField]
+    private float _attackInterval = 1f;
+
+    public int AttackValue
+    {
+        get => _attackValue;
+        set => _attackValue = value;
+    }
+    public float AttackInterval
+    {
+        get => _attackInterval;
+        set => _attackInterval = value;
+    }
+}
---
+++ Assets/Scripts/InGame/System/AttackSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class AttackSystem : SystemBase
+{
+    [SerializeField]
+    private List<AttackData> _attackDatas = default;
+    [SerializeField]
+    private int _defaultAttackValue = 1;
+
+    public override void Initialize(GameEvent gameEvent)
+    {
+        gameEvent.OnActivate += AddData;
+        gameEvent.OnDead += RemoveData;
+    }
+
+    public override void OnUpdate() { }
+
+    public override void OnDestroy(GameEvent gameEvent)
+    {
+        gameEvent.OnActivate -= AddData;
+        gameEvent.OnDead -= RemoveData;
+    }
+
+    private void AddData(GameObject go)
+    {
+        if (go.TryGetComponent(out AttackData attack))
+        {
+            _attackDatas.Add(attack);
+        }
+    }
+
+    private void RemoveData(GameObject go)
+    {
+        if (go.TryGetComponent(out AttackData attack))
+        {
+            _attackDatas.Remove(attack);
+        }
+    }
+}
